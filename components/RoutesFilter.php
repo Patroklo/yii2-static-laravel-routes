bbde598fbe1b6abbaf6044220db0145a488437c6
Patroklo <patroklo@gmail.com>
2015-12-18 16:22:40 +0100

    Changed bug in routes filter

--- components/RoutesFilter.php
+++ components/RoutesFilter.php
@@ -13,37 +13,73 @@ class RoutesFilter extends ActionFilter
 
     public function beforeAction($action)
     {
-        if (!($this->type == 'before')) {
+        if (!($this->type == 'before'))
+        {
             return TRUE;
         }
 
-        if (is_callable($this->rule)) {
+        if (is_callable($this->rule))
+        {
             $return_data = call_user_func($this->rule);
 
-            if (!is_bool($return_data)) {
+            if (!is_bool($return_data))
+            {
                 return FALSE;
             }
+
             return TRUE;
         }
+
         return FALSE;
     }
 
     public function afterAction($action, $result)
     {
-        if (!($this->type == 'after')) {
+        if (!($this->type == 'after'))
+        {
             return $result;
         }
 
-        if (is_callable($this->rule)) {
+        if (is_callable($this->rule))
+        {
             $return_data = call_user_func($this->rule);
 
-            if (!is_bool($return_data)) {
+            if (!is_bool($return_data))
+            {
                 return FALSE;
             }
+
             return $result;
         }
+
         return FALSE;
     }
 
-}
 
+    /**
+     * @inheritdoc
+     * @param \yii\base\Action $action
+     * @return bool
+     */
+    protected function isActive($action)
+    {
+        if ($this->owner instanceof Module)
+        {
+            // convert action uniqueId into an ID relative to the module
+            $mid = $this->owner->getUniqueId();
+            $id = $action->getUniqueId();
+            if ($mid !== '' && strpos($id, $mid) === 0)
+            {
+                $id = substr($id, strlen($mid) + 1);
+            }
+
+            $id = $action->controller->id . '/' . $id;
+        }
+        else
+        {
+            $id = $action->controller->id . '/' . $action->id;
+        }
+
+        return !in_array($id, $this->except, TRUE) && (empty($this->only) || in_array($id, $this->only, TRUE));
+    }
+}
